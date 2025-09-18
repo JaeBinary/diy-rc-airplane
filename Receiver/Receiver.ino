4293e4ba8b65a63c8cb0d0c33cf1e95a25695653
JBK <jaebin0815@naver.com>
2025-09-19 00:16:51 +0900

    Add basic code
    기본 송신기(Transmitter)와 수신기(Receiver) 프로그램입니다.
    nRF24L01+ 모듈을 통한 무선 통신을 구현합니다.

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,67 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// Define pins
+#define CE_PIN 9
+#define CSN_PIN 8
+
+// Create RF24 radio object
+RF24 radio(CE_PIN, CSN_PIN);
+
+// Address through which two modules communicate.
+const byte address[6] = "JBKPJ";
+
+struct MessageWithTimestamp {
+  int packetNumber;
+  unsigned long timestamp;
+  char message[20];
+};
+
+MessageWithTimestamp data;
+int cnt = 0;
+
+void setup() {
+  while (!Serial);
+  Serial.begin(9600);
+
+  // Power stabilization delay
+  Serial.println("Power stabilization...\n");
+  delay(3000);
+  
+  // Initialize the radio object
+  radio.begin();
+
+  // Ways to Improve the Range
+  radio.setChannel(108);                    // Change Channel Frequency (100~125)
+  radio.setDataRate(RF24_250KBPS);          // Use a Lower Data Rate (-94dBm)
+  radio.setPALevel(RF24_PA_MAX);            // Use of Higher Output Power (0dBm)
+
+  // Set the address
+  radio.openReadingPipe(0, address);        // Up to 6 pipes (0~5)
+  
+  // Set module as receiver
+  radio.startListening();
+
+  Serial.println("[Receiver Start]");
+  Serial.println("Packet No.\tTime(ms)\tMessage");
+  Serial.println("===========================================");
+}
+
+void loop() {
+  // Read the data if available in buffer
+  if (radio.available()) {
+    // Received data to Transmitter
+    radio.read(&data, sizeof(data));     // Up to 32 bytes (maximum packet)
+
+    // Debug message
+    Serial.print(data.packetNumber);
+    Serial.print("\t\t");
+    Serial.print(data.timestamp);
+    Serial.print("\t\t");
+    Serial.println(data.message);
+
+    cnt++;
+  }
+}
